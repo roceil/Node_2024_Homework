f2adad380be6852be2690faa6d6d835e4e2b66c7
roceil <a0978006326@gmail.com>
2024-05-05 21:27:35 +0800

    feat(*): 完成第四週作業

--- src/models/post-model.ts
+++ src/models/post-model.ts
@@ -1,45 +1,17 @@
 import mongoose from "mongoose"
 
-/**
- * 驗證文章標籤是否有至少一個
- * @param {string[]} tags - 文章標籤陣列
- */
-const validateTags = (tags: string[]): boolean => {
-  return tags.length > 0
-}
-
 const postSchema = new mongoose.Schema({
-  name: {
+  userId: {
     type: String,
-    required: [true, "Name 為必填欄位"]
+    required: [true, "User ID 為必填欄位"]
   },
-  tags: {
-    type: [String],
-    required: [true, "Tags 為必填欄位"],
-    validate: {
-      validator: validateTags,
-      message: "至少需要一個 tag"
-    }
-  },
-  type: {
+  contents: {
     type: String,
-    required: [true, "Type 為必填欄位"]
+    required: [true, "Contents 為必填欄位"]
   },
-  image: {
+  imgUrl: {
     type: String,
-    required: [true, "Image 為必填欄位"]
-  },
-  content: {
-    type: String,
-    required: [true, "Content 為必填欄位"]
-  },
-  likes: {
-    type: Number,
-    default: 0
-  },
-  comments: {
-    type: Number,
-    default: 0
+    default: null
   }
 }, {
   timestamps: true,
--- src/routes/post.ts
+++ src/routes/post.ts
@@ -1,17 +1,10 @@
 import { Router, type RequestHandler } from "express"
 import postController from "@/controllers/post-controller"
-import { checkId } from "@/middlewares/checkRequest"
 
 const router = Router()
 
 router.get("/posts", postController.getPosts as RequestHandler)
 
 router.post("/posts", postController.createPost as RequestHandler)
-
-router.delete("/posts", postController.deleteAllPosts as RequestHandler)
-
-router.delete("/post/:id", checkId, postController.deletePost as RequestHandler)
-
-router.put("/posts/:id", checkId, postController.editPost as RequestHandler)
 
 export default router
